b282169ccd933fae53908bd4001d6bd616de5517
TDDTDD <42080484+TDDTDD@users.noreply.github.com>
2020-11-30 15:07:17 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,16 +3,16 @@ import numpy as np
 import pickle as pk
 
 '''
-南京：
-1经度 = 92819.61471090886m
-1维度 = 111864.406779661m
-# child[i][j][k]:第i层第j个矩阵的第k个小矩阵
-# fa[i][j]:第i层第j个矩阵的父矩阵
-# num[i]:第i层的计数
-# areaShop[i][j]:最后一层第i个矩阵的第j个店铺
-# shopArea[i]:第i个店铺属于哪个矩阵
-# nameShop['肯德基']:店铺为肯德基的店铺id
-# shopName[23132]:店铺id为23132的店铺
+Nanjing：
+one longitude = 92819.61471090886m
+one latitude = 111864.406779661m
+child[i][j][k]:The kth child matrix of the i-th level j-th matrix
+fa[i][j]:The father matrix of the i-th level j-th matrix
+num[i]:The number of i-th level
+areaShop[i][j]:The last level, the jth store of the i-th matrix
+shopArea[i]:Which matrix does the i-th store belong to?
+nameShop['肯德基']:Store for KFC store id
+shopName[23132]:Stores with store id 23132
 '''
 child = np.full((15, 5000000, 5), -1, dtype=int)
 fa = np.full((25, 5000000), -1, dtype=int)
@@ -22,14 +22,14 @@ shopArea = {}
 nameShop = {}
 shopName = {}
 '''
-# 每个小矩阵在大矩阵中的位置,矩阵构造
+# The position of each small matrix in the larger matrix, matrix construction.
+# Row and Column Numbers
 # 1 2 ... 2048
 # 2
 # .
 # .
 # 2048
-# 最后矩阵经纬度差
-# Area[1] = [...] 每个小矩阵id对应的经纬度范围，便于验证正确性
+# Area[1] = [...] The latitude and longitude range for each sub-matrix id for easy verification of correctness.
 '''
 Square = {}
 longitude_difference = 0
@@ -37,7 +37,8 @@ latitude_difference = 0
 Area = {}
 
 
-# 给出矩阵对角线两点的经纬度和经纬度差 计算出迭代次数 和 最后的经纬度之差
+# Given the longitude and latitude of two points on the diagonal of the matrix and the difference between the last longitude and latitude,
+# calculate the number of iterations and the difference between the last longitude and latitude.
 def init(x_longitude_boundary, x_latitude_boundary,
          y_longitude_boundary, y_latitude_boundary, difference_boundary):
     global longitude_difference, latitude_difference
@@ -51,7 +52,7 @@ def init(x_longitude_boundary, x_latitude_boundary,
     return step
 
 
-# 划分区域
+# Subdivision of regions
 def distribute_data(x1, x2, y1, y2, now_step, nfa, x_latitude, x_longitude):
     fa[now_step][num[now_step]] = nfa
     if x1 == x2 and y1 == y2:
@@ -74,7 +75,7 @@ def distribute_data(x1, x2, y1, y2, now_step, nfa, x_latitude, x_longitude):
     return num[now_step] - 1
 
 
-# 将店铺划分到指定区域，并将店铺连接到对应的类型
+# Assign shops to specific zones and connect shops to the corresponding types.
 def distribute_shop(x_longitude_boundary, x_latitude_boundary, y_longitude_boundary, y_latitude_boundary,
                     now_longitude_difference, now_latitude_difference, now_data):
     dis_num = 0
@@ -82,20 +83,18 @@ def distribute_shop(x_longitude_boundary, x_latitude_boundary, y_longitude_bound
     for index, row in now_data.iterrows():
         longitude = row["longitude"]
         latitude = row["latitude"]
-        # 判断地点的合法性 并计算出当前地点属于哪个矩阵块
         if longitude < x_longitude_boundary or longitude > y_longitude_boundary or latitude > x_latitude_boundary or latitude < y_latitude_boundary:
             continue
         else:
             x_difference = x_latitude_boundary - latitude
             y_difference = longitude - x_longitude_boundary
-            # 余数大于等于0就加1 ，避免出现（0，0）
             dx = x_difference / now_latitude_difference - x_difference // now_latitude_difference
             dy = y_difference / now_longitude_difference - y_difference // now_longitude_difference
             x = x_difference // now_latitude_difference + (1 if dx >= 0.0 else 0)
             y = y_difference // now_longitude_difference + (1 if dy >= 0.0 else 0)
             area_id = Square[(x, y)]
 
-            # 检查连接的矩阵范围是否正确
+            # Check that the connected matrix range is correct
             if Area[area_id][0] <= latitude <= Area[area_id][1] and Area[area_id][2] <= longitude <= Area[area_id][3]:
                 check_num += 1
             else:
@@ -104,11 +103,11 @@ def distribute_shop(x_longitude_boundary, x_latitude_boundary, y_longitude_bound
 
             if check_num % 100000 == 0:
                 print("check num :" + str(check_num))
-            # 检查结束
+            # 
 
             areaShop[area_id].append(row["shop_id"])
             shopArea[row["shop_id"]] = area_id
-            # 将店铺id与店名相连
+            # Linking store id to store name
             if row["name"] not in nameShop:
                 nameShop[row["name"]] = [row["shop_id"]]
             else:
@@ -130,7 +129,7 @@ def data_process(x_longitude, x_latitude, y_longitude, y_latitude, difference_bo
     distribute_shop(x_longitude, x_latitude, y_longitude, y_latitude, longitude_difference, latitude_difference,
                     my_data)
     print("distribute success!")
-    # 存储
+    # store
     file = open(store_url, 'wb')
     pk.dump(child, file)
     pk.dump(fa, file)
@@ -146,7 +145,8 @@ def data_process(x_longitude, x_latitude, y_longitude, y_latitude, difference_bo
     print("store over!")
 
 
-# 传入要划分区域的左上角坐标，右下角坐标，经纬度差阈值，数据所在地址，结果存储地址
+# Passes the upper left corner coordinates, lower right corner coordinates, latitude/longitude difference threshold, address where the data is located,
+# and the address where the results are stored for the area to be divided.
 data_process(118.606465, 32.159281, 119.709680, 31.2438870, 0.001, 'data/shopData.csv', 'data/processData.pkl')
 '''
 # test:
